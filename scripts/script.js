764bc68d732d0b50e3078c7b33e0da328aa5faef
Andre <andrepaulinobtw@gmail.com>
2023-02-17 01:45:45 -0300

    Add js function to habit form

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,21 @@
-const habitsContainer = document.querySelector("#form-habits");
-
 const habitTracker = new NLWSetup(habitsContainer);
+const habitsContainer = document.querySelector("#form-habits");
+const addHabitBtn = document.querySelector("header button");
 
-const data = {
-	water: ["01-04", "01-05"],
-	food: ["01-01", "01-03"],
-	music: ["01-01", "01-02", "01-06"],
-};
+addHabitBtn.addEventListener("click", () => {
+	const today = new Date().toLocaleDateString("pt-br").slice(0, 5);
+	if (habitTracker.dayExists(today)) {
+		alert("Dia ja inserido.");
+		return;
+	}
 
+	habitTracker.addDay(today);
+});
+
+habitsContainer.addEventListener("change", () => {
+	localStorage.setItem("habitTrackerData", JSON.stringify(habitTracker.data));
+});
+
+const data = JSON.parse(localStorage.getItem("habitTrackerData")) || {};
 habitTracker.setData(data);
 habitTracker.load();
